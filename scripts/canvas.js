c8b802687d06d399c039ba5e8e6a87e6a65fea83
hanifan.nurahman <hanifan.nurahman@ibm.com>
2023-07-10 19:30:20 +0700

    move animation angel

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,23 +1,23 @@
-var space;
+let space;
 
 function floatySpace() {
-  var colors = ["#FF3F8E", "#04C2C9", "#2E55C1"];
+  let colors = ["#FF3F8E", "#04C2C9", "#2E55C1"];
 
   space = new CanvasSpace("canvas").display();
-  var form = new Form(space);
+  let form = new Form(space);
 
   // Elements
-  var pts = [];
-  var center = space.size.$divide(1.8);
-  var angle = -(window.innerWidth * 0.5);
-  var count = window.innerWidth * 0.05;
-  if (count > 150) count = 150;
-  var line = new Line(0, angle).to(space.size.x, 0);
-  var mouse = center.clone();
+  let pts = [];
+  let center = space.size.$divide(1.8);
+  let angle = -window.innerWidth;
+  let count = window.innerWidth * 5;
+  if (count > 144) count = 144;
+  let line = new Line(0, angle).to(space.size.x, 0);
+  let mouse = center.clone();
 
-  var r = Math.min(space.size.x, space.size.y) * 1;
-  for (var i = 0; i < count; i++) {
-    var p = new Vector(
+  let r = Math.min(space.size.x, space.size.y) * 1;
+  for (let i = 0; i < count; i++) {
+    let p = new Vector(
       Math.random() * r - Math.random() * r,
       Math.random() * r - Math.random() * r
     );
@@ -29,33 +29,33 @@ function floatySpace() {
   // Canvas
   space.add({
     animate: function (time, fps, context) {
-      for (var i = 0; i < pts.length; i++) {
+      for (let i = 0; i < pts.length; i++) {
         // rotate the points slowly
-        var pt = pts[i];
+        let pt = pts[i];
 
-        pt.rotate2D(Const.one_degree / 20, center);
+        pt.rotate2D(Const.one_degree / 34, center);
         form
           .stroke(false)
           .fill(colors[i % 3])
           .point(pt, 1);
 
         // get line from pt to the mouse line
-        var ln = new Line(pt).to(line.getPerpendicularFromPoint(pt));
+        let ln = new Line(pt).to(line.getPerpendicularFromPoint(pt));
 
         // opacity of line derived from distance to the line
-        var opacity = Math.min(
+        let opacity = Math.min(
           0.8,
           1 - Math.abs(line.getDistanceFromPoint(pt)) / r
         );
-        var distFromMouse = Math.abs(ln.getDistanceFromPoint(mouse));
+        let distFromMouse = Math.abs(ln.getDistanceFromPoint(mouse));
 
-        if (distFromMouse < 50) {
-          if (pts[i].brightness < 0.3) pts[i].brightness += 0.015;
+        if (distFromMouse < 55) {
+          if (pts[i].brightness < 0.3) pts[i].brightness += 0.013;
         } else {
           if (pts[i].brightness > 0.1) pts[i].brightness -= 0.01;
         }
 
-        var color = "rgba(255,255,255," + pts[i].brightness + ")";
+        let color = "rgba(255,255,255," + pts[i].brightness + ")";
         form.stroke(color).fill(true).line(ln);
       }
     },
